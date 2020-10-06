685a249e390abdb8489d3c80a151c8f9511757cd
willshuffy <saputrawilly26@gmail.com>
2020-10-06 07:45:10 +0700

    Intent Object

--- app/src/main/java/com/willshuffyproject/parzelize/MainActivity.kt
+++ app/src/main/java/com/willshuffyproject/parzelize/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.willshuffyproject.parzelize
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMoveWithObject:Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveWithObject.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+
+        when(v.id){
+
+
+            R.id.btn_move_activity_object -> {
+
+                val person = Person(
+
+                    "DicodingAcademy",
+                    5,
+                    "academy@dicoding.com",
+                    "Bandung"
+                )
+
+                val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithObjectIntent)
+            }
+        }
+
     }
 }
+
+
